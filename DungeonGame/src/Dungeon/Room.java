9a2f3aa9bd80ebb4b24e547bcb4cb4371c36b1e7
Craig Smith <craigshmitty6@gmail.com>
2023-03-16 08:36:32 -0400

    [ADD & MOD] Added Combat class for managing combat between players and monsters. Modified Room class to have three data fields, id for room id, and two booleans that track whether a monster or a player is in the room

--- DungeonGame/src/Dungeon/Room.java
+++ DungeonGame/src/Dungeon/Room.java
@@ -1,4 +1,24 @@
 package Dungeon;
 
 public class Room {
+
+    public int id;
+    public boolean playerHere;
+    public boolean monsterHere;
+
+    public boolean isPlayerHere() {
+        return playerHere;
+    }
+
+    public void setPlayerHere(boolean playerHere) {
+        this.playerHere = playerHere;
+    }
+
+    public boolean isMonsterHere() {
+        return monsterHere;
+    }
+
+    public void setMonsterHere(boolean monsterHere) {
+        this.monsterHere = monsterHere;
+    }
 }
